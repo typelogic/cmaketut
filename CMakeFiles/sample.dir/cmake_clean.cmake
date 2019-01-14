2e897b6e67cf3ace6b78d4cae052b7bce7d8d4e6
truthadjustr <da185157@ncr.com>
2019-01-14 21:23:18 +0800

    after 'cmake .'

---
+++ CMakeFiles/sample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample.dir/testapp.cpp.o"
+  "sample.pdb"
+  "sample.exe"
+  "libsample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
